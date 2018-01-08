68598736fd64c43ee08e9de07fa3351b3c01645c
dmitriy_ee <dmitriy_ee@hotmail.com>
2018-01-08 22:27:28 +0200

    display cart list

--- app_1.rb
+++ app_1.rb
@@ -21,7 +21,13 @@ end
 post '/cart' do
 	#@orders = params[:orders]
 	orders_input = params[:orders]
-	@orders = parse_orders_input orders_input
+	@items = parse_orders_input orders_input
+
+	@items.each do |item|
+		#id, cnt
+		item[0] = Product.find(item[0])
+	end
+	
     erb :cart
 end
 
